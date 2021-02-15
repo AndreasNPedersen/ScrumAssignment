0296d0f69ad4d4cfc3217ed49cc45391545c7cfe
AlexTriumf <TriumfAlex@gmail.com>
2021-02-15 13:42:33 +0100

    handler json data

---
+++ SCRUMAssignment/Data/JsonEmne.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+using SCRUMAssignment.Models;
+
+namespace SCRUMAssignment.Data
+{
+    public class JsonEmne
+    {
+        public Dictionary<int, Emne> ReadJsonFile(string filePath)
+        {
+            string input = File.ReadAllText(filePath);
+            return JsonConvert.DeserializeObject<Dictionary<int, Emne>>(input);
+        }
+
+        public void WriteJsonFile(Dictionary<int, Emne> dic, string filePath)
+        {
+            string output = JsonConvert.SerializeObject(dic, Formatting.Indented);
+            File.WriteAllText(filePath, output);
+        }
+    }
+}
+
---
+++ SCRUMAssignment/Services/EmnerHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SCRUMAssignment.Data;
+using SCRUMAssignment.Models;
+
+namespace SCRUMAssignment.Services
+{
+    public class EmneHandler
+    {
+
+        private string filePath = @"\Data\DataEmne.json";
+
+        public void Create(Emne emne)
+        {
+            JsonEmne jsonEmne = new JsonEmne();
+            Dictionary<int, Emne> dic = jsonEmne.ReadJsonFile(filePath);
+            dic.Add(emne.Id, emne);
+            jsonEmne.WriteJsonFile(dic, filePath);
+        }
+
+        public void Delete(int id)
+        {
+            JsonEmne jsonEmne = new JsonEmne();
+            Dictionary<int, Emne> dic = jsonEmne.ReadJsonFile(filePath);
+            dic.Remove(id);
+            jsonEmne.WriteJsonFile(dic, filePath);
+        }
+
+        public Dictionary<int, Emne> FilterDictionary(int filter)
+        {
+            JsonEmne jsonEmne = new JsonEmne();
+            Dictionary<int, Emne> dic = jsonEmne.ReadJsonFile(filePath);
+            Dictionary<int, Emne> dicC = new Dictionary<int, Emne>();
+            foreach (Emne emne in dic.Values)
+            {
+                {
+                    dicC.Add(emne.Id, emne);
+                }
+               
+            }
+            return dicC;
+        }
+
+        public Emne Get(int id)
+        {
+            JsonEmne jsonEmne = new JsonEmne();
+            Dictionary<int, Emne> dic = jsonEmne.ReadJsonFile(filePath);
+            return dic[id];
+        }
+
+        public Dictionary<int, Emne> GetDictionary()
+        {
+            JsonEmne jsonEmne = new JsonEmne();
+            return jsonEmne.ReadJsonFile(filePath);
+
+        }
+
+       
+    }
+}
+
